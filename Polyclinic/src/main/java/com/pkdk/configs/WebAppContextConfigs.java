d20d8d681df4ecd7f79a3650925678cbce9614da
hungthai123 <thaihung.work05@gmail.com>
2026-04-01 10:16:46 +0700

    New project

---
+++ Polyclinic/src/main/java/com/pkdk/configs/WebAppContextConfigs.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pkdk.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Admin
+ */
+@Configuration
+@ComponentScan(
+        basePackages = {
+            "com.pkdk.controllers"
+        }
+)
+@EnableWebMvc
+public class WebAppContextConfigs implements WebMvcConfigurer{
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+        
+    }
+    
+}
